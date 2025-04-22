c06fff94ac60aa37e5fe937d787c457c0d69a2d4
YHL <1241171248@qq.com>
2025-04-22 19:28:23 +0800

    lab3 修正

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -22,7 +22,7 @@ public class TimeAList {
     }
 
     public static void timeAListConstruction() {
-        int[] nList = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 2560000, 51200000};
+        int[] nList = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
         AList<Integer> ns = new AList<>();
         AList<Double> times = new AList<>();
         AList<Integer> opCounts = new AList<>();
