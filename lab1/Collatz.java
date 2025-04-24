a90d1bf5c770513bcb7c6f340a02b948dfc33a2a
YHL <1241171248@qq.com>
2025-04-24 18:24:20 +0800

    lab4 restore the lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,15 +5,8 @@ public class Collatz {
 
     /** The implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
-//        if (n % 2 == 0) return n / 2;
-//        else return 3 * n + 1;
+        if (n % 2 == 0) return n / 2;
+        else return 3 * n + 1;
     }
 
     public static void main(String[] args) {
