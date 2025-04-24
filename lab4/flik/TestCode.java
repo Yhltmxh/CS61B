a5b91b769dc0c3355ee15ee00d5cb9edb481d539
YHL <1241171248@qq.com>
2025-04-24 18:49:22 +0800

    lab4 Debugging Mystery finished

---
+++ lab4/flik/TestCode.java
@@ -0,0 +1,18 @@
+package flik;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @description: Test the code
+ * @author: 杨怀龙
+ * @create: 2025-04-24 18:32
+ **/
+public class TestCode {
+
+    @Test
+    public void testIsSameNumber() {
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+}
